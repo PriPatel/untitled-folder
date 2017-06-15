16b674914910072f88478fc05cf19d7198089e7a
Pri <pridawg@Pridawgs-MacBook-Pro.local>
2017-06-15 18:22:58 +0100

    HEY TEST

---
+++ sites-interactive.js
@@ -0,0 +1,30 @@
+
+
+
+var colour1 = $('#colour1');
+
+var colour2 = $('#colour2');
+
+var colour3 = $('#colour3');
+
+var colour4 = $('#colour4');
+
+var colour5 = $('#colour5');
+
+var colour6 = $('#colour6');
+
+var colour7 = $('#colour7');
+
+var colour8 = $('#colour8');
+
+var colour9 = $('#colour9');
+
+var header = $('.stage-header');
+
+
+ 
+
+
+        colour1.click(function(){
+    header.css("background-color", "red");
+});
